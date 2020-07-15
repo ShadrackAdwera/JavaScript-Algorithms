3487d72ca3fd625c02bc1591ff21b1deb37932a3
FACTS  LIMITED <admin@factsafrica.com>
2020-07-15 10:47:00 +0300

    use a bitwise operator with the power of two

--- isPowerOfTwo.js
+++ isPowerOfTwo.js
@@ -1,24 +1,40 @@
+// const isPowerOfTwo = (n) => {
+//     let dividedNumber = n
+//     if(n<1) {
+//       return false
+//     }
+//     while(dividedNumber !==1) {
+//       if(dividedNumber % 2 !==0) {
+//         return false
+//       }
+//       dividedNumber = dividedNumber / 2
+//     }
+//     return true
+//   }
+
+//   //Best Case: Number not divisible by 2 => Constant Time Complexity: O(1)
+//   //Average Case: Number divisible by  2 but not a power of 2 => Logarithmic Time Complexity => O(log n)
+//   //Worst Case: Large number that is a power of two => Logarithmic Time Complexity => O(log n) eg. 1,125,899,906,842,624
+
+//   //Best Case
+//   console.log(isPowerOfTwo(7))
+//   //Average Case
+//   console.log(isPowerOfTwo(36))
+//   //worst case ie. 2^50
+//   console.log(isPowerOfTwo(1125899906842624))
+
+//SOLUTION TWO: Bitwise 'and'
+
 const isPowerOfTwo = (n) => {
-    let dividedNumber = n
-    if(n<1) {
-      return false
-    }
-    while(dividedNumber !==1) {
-      if(dividedNumber % 2 !==0) {
-        return false
-      }
-      dividedNumber = dividedNumber / 2
-    }
-    return true
+  if (n < 1) {
+    return false;
   }
-  
-  //Best Case: Number not divisible by 2 => Constant Time Complexity: O(1)
-  //Average Case: Number divisible by  2 but not a power of 2 => Logarithmic Time Complexity => O(log n)
-  //Worst Case: Large number that is a power of two => Logarithmic Time Complexity => O(log n) eg. 1,125,899,906,842,624
-  
-  //Best Case
-  console.log(isPowerOfTwo(7))
-  //Average Case
-  console.log(isPowerOfTwo(36))
-  //worst case ie. 2^50
-  console.log(isPowerOfTwo(1125899906842624))
+  return (n & (n - 1))===0;
+};
+
+//   //Best Case
+   console.log(isPowerOfTwo(7))
+//   //Average Case
+   console.log(isPowerOfTwo(36))
+//   //worst case ie. 2^50
+   console.log(isPowerOfTwo(1125899906842624))
