080fb8d4a4853f9bb66fde6cf86b5a6eb3ee5cf0
FACTS  LIMITED <admin@factsafrica.com>
2020-07-15 11:28:33 +0300

    factorial of a number using recursion

--- factorial.js
+++ factorial.js
@@ -1,12 +1,31 @@
-const factorial = n => {
-    let product = 1
-    if(n<=1) {
-        return 1
-    }
-    for(let i =1; i<=n; i++) {
-        product = product * i
-    }
-    return product
-}
+const factorial = (n) => {
+  let product = 1;
+  if (n <= 1) {
+    return 1;
+  }
+  for (let i = 1; i <= n; i++) {
+    product = product * i;
+  }
+  return product;
+};
 
-console.log(factorial(2))
+//Linear complexity O(n)
+console.log(factorial(3));
+console.log(factorial(4));
+console.log(factorial(5));
+
+
+//SOLUTION 2 : RECURSION
+
+const factorial = n => {
+    if(n===1) {
+      return 1
+    }
+    return n * factorial(n-1)
+  }
+  
+  //In every function call, O(1) but function is triggered n times = 1*n = n
+  //Linear complexity O(n)
+  console.log(factorial(3))
+  console.log(factorial(4))
+  console.log(factorial(5))
