a166021e2812e709e655038df5c724d304c37e91
FACTS  LIMITED <admin@factsafrica.com>
2020-07-16 14:11:41 +0300

    binary search using loops

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 let counter = 0
 const searchNums = (arry, num) => {
-    for(let i = 0; i<arry.length; i++){
-        counter++
-        if(arry[i]==num){
-            return i
-        }
-    }
+   let sortedArray = arry.sort()
+
+   let startingIndex = 0
+   let endIndex = sortedArray.length - 1
+
+   while(startingIndex<=endIndex) {
+       let middleIndex = startingIndex + Math.floor((endIndex - startingIndex)/2)
+       if(sortedArray[middleIndex]===num){
+           return middleIndex
+       }
+
+       if(sortedArray[middleIndex] > num) {
+        endIndex = middleIndex - 1
+       } else {
+           startingIndex = middleIndex + 1
+       }
+   }
+    
 }
 
-console.log(searchNums([1,2,3,4,5,4,5,7,3,5,78,1,2,3], 78))
-console.log(counter)
+const arry = [91,93,96,84,93,82,66,21,8,61,34,42,58,3,74,44,67,16,36,76,29,64,49,32,5,17,79,64,71,29]
+
+console.log(searchNums(arry, 71))
+
+//Best Case Scenario : O(1) => Constant Time Complexity: Element in the middle
+
+//Average Case Scenario : O(log n) : Logarithmic Time- Split the array in half with every iteration : Element anywhere in the array
+
+//Worst Case Scenario : O(log n) : Logarithmic Time - Split the array in half with every iteration =>  : Element at the beginning or the end
