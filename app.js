a7dd320d00cc88a36e6f5d695a0912f8722b6b56
FACTS  LIMITED <admin@factsafrica.com>
2020-07-19 16:38:52 +0300

    cartesian product algorithm

--- app.js
+++ app.js
@@ -1,39 +1,15 @@
-//-----------------
-//SOLUTION 1: LOOPS
-//-----------------
-
-const factorial = n => {
-    let product = 1
-    for(let i = 2; i<=n; i++) {
-        product *= i
+const cartProduct = (setA, setB) => {
+    let product = []
+    for(const setAElem of setA) {
+        for(const setBElem of setB) {
+            product.push([setAElem, setBElem])
+        }
     }
     return product
 }
 
 
-console.log(factorial(4))
+const colors = ['Blue','Red','Yellow', 'Black', 'White','Brown']
+const sizes = ['xs','S','M','L','XL']
 
-//Step 1: Find places where data(values) is stored permanently in the algorithm
-
-//Step 2: Count how often 'permanently stored values' are being created and kept around
-
-//Space COmplexity O(1) : Operation is done on the same number throughout the loop iteration and no new values('permanent') are created
-
-//----------------------
-//SOLUTION 2 : Recursion
-//----------------------
-
-const fact = n => {
-    if(n<=1) {
-        return 1
-    }
-    return n * fact(n-1)
-}
-
-//Space Complexity : O(n)
-//A new value is created for every nested function call - the parameter received
-
-
-
-
-
+console.log(cartProduct(colors, sizes))
