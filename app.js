2e8f2b904c924a89bb1bb8799423a2de36ef6469
FACTS  LIMITED <admin@factsafrica.com>
2020-07-22 11:14:34 +0300

    Permutation without repetition

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-const cartProduct = (setA, setB) => {
-    let product = []
-    for(let setAElem of setA) {
-        if(!Array.isArray(setAElem)) {
-            setAElem = [setAElem]
-        }
-        for(const setBElem of setB) {
-            product.push([...setAElem, setBElem])
+//Permutations: An ordered combination of values
+// Types: Without and with repetition
+
+//Without Repetition: eg. TO-DO List : Multiple combinations with every task done once
+//With Repetition: eg. Password Combination: Multiple combinations with values allowed to repeat themselves
+
+const getPermutations = arry => {
+    const permutations = []
+    if(arry.length===1) {
+        return [arry]
+    }
+
+    const partialPermutations = getPermutations(arry.slice(1))
+
+    const firstOption = arry[0]
+
+    for(let i = 0; i<partialPermutations.length; i++) {
+        const partialPermutation = partialPermutations[i]
+        for(let j = 0; j<=partialPermutation.length; j++) {
+            const permInfront = partialPermutation.slice(0,j)
+            const permAfter = partialPermutation.slice(j)
+            permutations.push(permInfront.concat([firstOption],permAfter))
         }
     }
-    return product
+    return permutations
 }
 
-const cartesianProduct = (...params) => {
-    let tempProduct = params[0]
-    for(let i = 1; i<params.length; i++) {
-        tempProduct = cartProduct(tempProduct, params[i])
-    }
-    return tempProduct
-}
+const toDo = ['Pretend to work','Try to look busy','Go home','Go for a run']
 
-
-const colors = ['Blue','Red','Yellow', 'Black', 'White','Brown']
-const sizes = ['xs','S','M','L','XL']
-const style = ['round neck','v-neck','button']
-
-console.log(cartesianProduct(colors,sizes,style))
-
-
-//Time Complexity: O(n*m)
-//Space Complexity: O(n*m)
+console.log(getPermutations(toDo))
---
+++ sets-algorithms/permutations.js
@@ -0,0 +1,5 @@
+//Permutations: An ordered combination of values
+// Types: Without and with repetition
+
+//Without Repetition: eg. TO-DO List : Multiple combinations with every task done once
+//With Repetition: eg. Password Combination: 
